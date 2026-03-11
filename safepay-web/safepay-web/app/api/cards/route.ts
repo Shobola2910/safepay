3e0a48726dfffd794d8f071b32bc96fd5cf2ec4e
Shobola <shorustamovismoilbek2@gmail.com>
2026-03-11 19:18:10 -0400

    Delete safepay-web/safepay-web directory

--- safepay-web/safepay-web/app/api/cards/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { cards } from "@/lib/mock-data";
-
-export async function GET() {
-  return NextResponse.json({ data: cards, count: cards.length });
-}
