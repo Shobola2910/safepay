7280523dd50e51f451870486461b481a0f3db0e3
Shobola <shorustamovismoilbek2@gmail.com>
2026-03-11 19:49:52 -0400

    Delete app directory

--- app/api/transactions/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { transactions } from "@/lib/mock-data";
-
-export async function GET() {
-  return NextResponse.json({ data: transactions, count: transactions.length });
-}
