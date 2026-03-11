f77a9e1ba437be6be9cc3fc4ad6c9a62e5935e52
Shobola <shorustamovismoilbek2@gmail.com>
2026-03-11 19:19:58 -0400

    Delete safepay-web directory

--- safepay-web/app/api/devices/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { devices } from "@/lib/mock-data";
-
-export async function GET() {
-  return NextResponse.json({ data: devices, count: devices.length });
-}
